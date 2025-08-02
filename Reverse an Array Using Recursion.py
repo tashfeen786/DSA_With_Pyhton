12a47388add53f8d89d721ef5287e963b2acc85c
tashfeen786 <tashfeenakhonzada786@gmail.com>
2025-08-03 00:07:24 +0500

    Update Reverse an Array Using Recursion.py

--- Reverse an Array Using Recursion.py
+++ Reverse an Array Using Recursion.py
@@ -1,7 +1,11 @@
 num = [5, 7, 3,9,5,3, 8,9, 2, 1]
-left = 0
-right = len(num) - 1
-num[left], num[right] = num[right], num[left]
-left += 1
-right -= 1  
+def func(num, left, right):
+    if left >= right:
+        return
+    arr[left], arr[right] = arr[right], arr[left]
+    func(arr, left + 1, right - 1)
+def reverse_array(num, left, right):
+    self.func(num, 0, len(num) - 1)
+    return num
 
+print(num)
